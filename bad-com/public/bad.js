7d9d6891b3b8f495bc910ecbec308b11146ca2e3
MiManchi <2237268069@qq.com>
2022-04-17 19:22:57 +0800

    2022.4.17 跨域操作（CORS  JSONP）

---
+++ bad-com/public/bad.js
@@ -0,0 +1,23 @@
+function jsonp(url) {
+    return new Promise((resolve, reject) => {
+        const random = 'MiManchiCallback' + Math.random()
+        window[random] = data => {
+            resolve(data)
+        }
+        const script = document.createElement('script')
+        script.src = `${url}?callback=${random}`
+        script.onload = () => {
+            script.remove()
+        }
+        script.onerror = () => {
+            reject()
+        }
+        document.body.appendChild(script)
+    })
+}
+jsonp('http://localhost:8888/friends.js')
+    .then((data) => {
+        console.log(data)
+    })
+
+//请求friends.js
